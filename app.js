9b8b6bc11b14c6e35a14a37c1b748e8d44536530
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-13 16:02:49 +0100

    extracting blog to models folder

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 const { MONGODB_URI } = require("./utils/config");
 
 const app = express();
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 mongoose.connect(MONGODB_URI);
-
 app.use(cors());
 app.use(express.json());
 
