87f826ac18e89f45911096828d9ccd46fce8490b
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-13 16:44:18 +0100

    covering edge cases

--- tests/totalLikes.test.js
+++ tests/totalLikes.test.js
@@ -63,6 +63,16 @@ describe("totalLikes unit tests", () => {
     },
   ];
 
+  test("when list is null, then return 0", () => {
+    const result = totalLikes(null);
+    expect(result).toBe(0);
+  });
+
+  test("when list has no blogs, then return 0", () => {
+    const result = totalLikes([]);
+    expect(result).toBe(0);
+  });
+
   test("when list only has one blog then total likes eq likes of that blog", () => {
     const result = totalLikes(listWithOneBlog);
     expect(result).toBe(1);
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,5 +1,10 @@
 const dummy = (blogs) => 1;
 
-const totalLikes = (blogs) => 1;
+const totalLikes = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return 0;
+  }
+  return 1;
+};
 
 module.exports = { dummy, totalLikes };
