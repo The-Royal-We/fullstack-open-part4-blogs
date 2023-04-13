114b0209350129a2b1b5babb9a96c10e049a4830
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-13 17:10:25 +0100

    complete exercise 4.5

--- tests/favoriteBlog.test.js
+++ tests/favoriteBlog.test.js
@@ -1,4 +1,5 @@
 const { favoriteBlog } = require("../utils/list_helper");
+const { listWithOneBlog, listWithManyBlogs } = require("./testHelpers");
 
 describe("favoriteBlog unit tests", () => {
   test("when list is null then return null", () => {
@@ -10,8 +11,20 @@ describe("favoriteBlog unit tests", () => {
     expect(result).toBeNull();
   });
 
-  xtest("when list is contains one blog then return that blog", () => {
-    const result = favoriteBlog(null);
-    expect(result).toStrictEqual([]);
+  test("when list contains one blog then return that blog", () => {
+    const result = favoriteBlog(listWithOneBlog);
+    expect(result).toBe(listWithOneBlog[0]);
+  });
+
+  test("when list contains many blogs then return blog with most likes", () => {
+    const result = favoriteBlog(listWithManyBlogs);
+    expect(result).toStrictEqual({
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    });
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,6 +8,15 @@ const totalLikes = (blogs) => {
   return blogs.map((b) => b.likes).reduce((a, b) => a + b, 0);
 };
 
-const favoriteBlog = (_blogs) => null;
+const favoriteBlog = (blogs) => {
+  if (!blogs || blogs.length === 0) {
+    return null;
+  }
+  return blogs.reduce(
+    (currentMostLiked, blog) =>
+      blog.likes > currentMostLiked.likes ? blog : currentMostLiked,
+    { likes: -1000 }
+  );
+};
 
 module.exports = { dummy, totalLikes, favoriteBlog };
